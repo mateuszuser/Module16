7b5024d1d32d7b7c22561499365065f9ce60744c
mateuszuser <ciomcia.mateusz@gmail.com>
2022-08-17 11:20:13 +0200

    task 16.2

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=blog
+flask run
--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,44 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('base.html')
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+def create_edit_entry(entry_id = None):
+    if entry_id == None:
+        form = EntryForm()
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():               
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                    )
+                db.session.add(entry)
+                db.session.commit()
+            else:
+                errors = form.errors
+    else:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        errors = None
+        if request.method == 'POST':
+            if form.validate_on_submit():                      
+                form.populate_obj(entry)
+                db.session.commit()                 
+            else:
+                errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def call_function_create_entry():
+    return create_edit_entry()
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def call_function_edit_entry(entry_id):
+    return create_edit_entry(entry_id)
